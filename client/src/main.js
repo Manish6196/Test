72809aa4e0504fd224bf888f9f53e0506f13ca28
manish <manishrajput6196@gmail.com>
2019-06-11 14:51:13 +0530

    "manish updated project"

--- client/src/main.js
+++ client/src/main.js
@@ -5,6 +5,9 @@ import store from './store'
 import VeeValidate from "vee-validate";
 import 'ant-design-vue/dist/antd.css'; 
 import antd from 'ant-design-vue';
+import Axios from 'axios';
+
+Axios.defaults.baseURL = "https://92832de0.ngrok.io/"
 
 Vue.use(VeeValidate);
 Vue.use(antd)
